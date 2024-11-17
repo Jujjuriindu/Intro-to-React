29dcbfb79e3c6bb7abc1ee09cc8c85fa3cfd3baf
Jujjuriindu <140819241+Jujjuriindu@users.noreply.github.com>
2024-11-17 13:03:26 +0530

    Add files via upload

---
+++ Intro to JS/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Browser understands three things are HTML, CSS, Java Script</h1>
+      <h2>JS Library is nothing but react JS</h2>
+      <h3>JS library is a collection of JS Files</h3>
+    </div>
+  );
+}
+
+export default App;
